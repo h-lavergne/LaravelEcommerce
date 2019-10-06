03d5522aa6111c60ef2dcda05ba94946587f5012
Lavergne <hugo.lavergne@devinci.fr>
2019-10-07 01:24:58 +0200

    configure models & migration & seeds for db

---
+++ app/Models/Admin.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Model
+{
+    use Notifiable;
+
+
+    protected $fillable = ["name", "email", "password"];
+
+    protected $hidden = ["password", "remember-token"];
+
+    protected $casts = ["email_verified_at" => 'datetime'];
+}
